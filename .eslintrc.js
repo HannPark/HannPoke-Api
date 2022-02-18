a1b097b05f8514b8c1e6417f6ebe5eb7a6bb2811
Jose Ojeda <jlojeda0903@gmail.com>
2022-02-17 20:32:09 -0500

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const dotenv = require("dotenv");
+const express = require("express");
+const morgan = require("morgan"); // MIDDLEWARE
+const cors = require("cors");
+
+dotenv.config();
+
+// conectarse a DB
+// const connectDatabase = require("./config/db");
+// connectDatabase();
+
+// Instancias de Rutas
+
+const app = express();
+app.use(express.json()); // Let Express Handle JSON
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Rutas de Backend
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log("Servidor se ejecuta en ambiente", process.env.NODE_ENV),
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log("Errores:", err.message);
+  server.close(() => process.exit(1));
+});
